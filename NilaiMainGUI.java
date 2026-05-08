36c6b6f7a7c41298dc948e6a175e4928d7c2cede
michaellll <michaelbjn9@gmail.com>
2026-05-08 16:33:12 +0700

    NilaiMainGUI.java

--- NilaiMainGUI.java
+++ NilaiMainGUI.java
@@ -1,13 +1,8 @@
 public class NilaiMainGUI {
-
     public static void main(String[] args) {
-
         NilaiGUI window = new NilaiGUI();
-
         window.setTitle("Hitung Nilai Akhir dengan GUI Java AWT");
-
         window.setSize(420, 600);
-
         window.setVisible(true);
     }
 }
